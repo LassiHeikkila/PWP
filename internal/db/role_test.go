0e27dfac80ab22cdb132565a8c49c537ba37f5c4
Lassi Heikkilä <lassi.m.heikkila@gmail.com>
2022-02-09 07:31:04 +0200

    Add some more role tests

--- internal/db/role_test.go
+++ internal/db/role_test.go
@@ -17,8 +17,58 @@ func TestHasRole(t *testing.T) {
 		},
 		"1": {
 			in:   RoleUser | RoleMaintainer | RoleAdministrator,
+			b:    RoleMaintainer,
+			want: true,
+		},
+		"2": {
+			in:   RoleUser | RoleMaintainer | RoleAdministrator,
+			b:    RoleAdministrator,
+			want: true,
+		},
+		"3": {
+			in:   RoleUser | RoleMaintainer | RoleAdministrator,
 			b:    RoleRoot,
 			want: false,
+		},
+		"4": {
+			in:   RoleUser | RoleMaintainer,
+			b:    RoleUser,
+			want: true,
+		},
+		"5": {
+			in:   RoleUser | RoleMaintainer,
+			b:    RoleMaintainer,
+			want: true,
+		},
+		"6": {
+			in:   RoleUser | RoleMaintainer,
+			b:    RoleAdministrator,
+			want: false,
+		},
+		"7": {
+			in:   RoleUser | RoleMaintainer,
+			b:    RoleRoot,
+			want: false,
+		},
+		"8": {
+			in:   RoleRoot,
+			b:    RoleUser,
+			want: false,
+		},
+		"9": {
+			in:   RoleRoot,
+			b:    RoleMaintainer,
+			want: false,
+		},
+		"10": {
+			in:   RoleRoot,
+			b:    RoleAdministrator,
+			want: false,
+		},
+		"11": {
+			in:   RoleRoot,
+			b:    RoleRoot,
+			want: true,
 		},
 	}
 
