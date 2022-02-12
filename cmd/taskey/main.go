84f21b403c437893fd067b05e192db9835a1974f
Lassi Heikkilä <lassi.m.heikkila@gmail.com>
2022-02-12 10:14:36 +0200

    Make taskey cmd connect to Postgres db and initialize the tables

--- cmd/taskey/main.go
+++ cmd/taskey/main.go
@@ -1,9 +1,60 @@
 package main
 
 import (
-	"fmt"
+	"context"
+	"log"
+	"os"
+	"os/signal"
+
+	"github.com/LassiHeikkila/taskey/internal/db"
+)
+
+const (
+	postgresHost = "127.0.0.1"
+	postgresPort = 5432
+	postgresUser = "postgres"
+	postgresPw   = "test1234"
 )
 
 func main() {
-	fmt.Println("I don't do anything yet")
+	ctx, cancel := context.WithCancel(context.Background())
+	go func() {
+		sc := make(chan os.Signal, 1)
+		signal.Notify(sc, os.Interrupt)
+		<-sc
+		log.Println("caught interrupt signal")
+		cancel()
+	}()
+	s := run(ctx)
+	os.Exit(s)
+}
+
+func run(ctx context.Context) int {
+	d := db.OpenDB(
+		db.WithHost(postgresHost),
+		db.WithPort(postgresPort),
+		db.WithUsername(postgresUser),
+		db.WithPassword(postgresPw),
+		db.WithSSLMode("disable"),
+	)
+	if d == nil {
+		log.Println("failed to initialize database!")
+		return 1
+	}
+
+	if err := db.InitializeDB(d); err != nil {
+		log.Println("error initalizing db:", err)
+		return 1
+	}
+
+	c := db.NewController(d)
+	if c == nil {
+		log.Println("failed to create db controller!")
+		return 1
+	}
+
+	log.Println("db initialized")
+
+	<-ctx.Done()
+	return 0
 }
