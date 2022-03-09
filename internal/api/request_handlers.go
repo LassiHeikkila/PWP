a0850171963b13332bc342feab8dcab67205012b
Lassi Heikkilä <lassi.m.heikkila@gmail.com>
2022-03-09 19:32:51 +0200

    Add readUsers handler to read all users in an organization and return "unimplemented" text from all unimplemented handlers rather than service unavailable

--- internal/api/request_handlers.go
+++ internal/api/request_handlers.go
@@ -13,37 +13,43 @@ import (
 func (h *handler) signupHandler(w http.ResponseWriter, req *http.Request) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) createOrganization(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) readOrganization(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) updateOrganization(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) deleteOrganization(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) createUser(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
+}
+
+func (h *handler) readUsers(w http.ResponseWriter, req *http.Request, user *types.User) {
+	// TODO: implement
+	defer req.Body.Close()
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 // /api/v1/{organization_id}/users/{user_id}/
@@ -80,155 +86,155 @@ func (h *handler) readUser(w http.ResponseWriter, req *http.Request, caller *typ
 func (h *handler) updateUser(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) deleteUser(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) createUserToken(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) deleteUserToken(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) createMachine(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) readMachine(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) updateMachine(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) deleteMachine(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) createMachineToken(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) deleteMachineToken(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) createMachineSchedule(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) readMachineSchedule(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) updateMachineSchedule(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) deleteMachineSchedule(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) addRecord(w http.ResponseWriter, req *http.Request, machine *types.Machine) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) getRecord(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) getRecords(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) deleteRecord(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) createTask(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) readTask(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) readTasks(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) updateTask(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) deleteTask(w http.ResponseWriter, req *http.Request, user *types.User) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) loginHandler(w http.ResponseWriter, req *http.Request) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) loginChecker(w http.ResponseWriter, req *http.Request) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
 
 func (h *handler) passwordChangeHandler(w http.ResponseWriter, req *http.Request) {
 	// TODO: implement
 	defer req.Body.Close()
-	http.Error(w, http.StatusText(http.StatusServiceUnavailable), http.StatusServiceUnavailable)
+	http.Error(w, "unimplemented", http.StatusServiceUnavailable)
 }
