da0b35abecce506986d761d739f262c9ff4f0d08
Lassi Heikkilä <lassi.m.heikkila@gmail.com>
2022-03-13 13:14:31 +0200

    Dont name unused argument

--- internal/auth/uuid.go
+++ internal/auth/uuid.go
@@ -4,7 +4,7 @@ import (
 	"github.com/google/uuid"
 )
 
-func (a *authController) GenerateUUID() (string, error) {
+func (_ *authController) GenerateUUID() (string, error) {
 	u, err := uuid.NewRandom()
 	if err != nil {
 		return "", err
