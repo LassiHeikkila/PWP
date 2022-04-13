5d4496650902b99adacb473577ef0e5a78de17ad
Lassi Heikkilä <lassi.m.heikkila@gmail.com>
2022-04-13 20:17:33 +0300

    Set Content-Type: application/json header in health endpoint

--- cmd/taskey/health.go
+++ cmd/taskey/health.go
@@ -9,7 +9,7 @@ func serveHealth(w http.ResponseWriter, _ *http.Request) {
 	m := map[string]interface{}{
 		"ok": true,
 	}
-
+	w.Header().Set("Content-Type", "application/json")
 	enc := json.NewEncoder(w)
 	_ = enc.Encode(&m)
 }
